4e5fac01955911e2e3effb2897c1181f9c633004
Raza Hanif <raza_hanif@ymail.com>
2025-04-11 15:52:49 -0400

    OhGit Auto-Commit @ 3:52:49 PM

--- frontend/SqueekoApp/src/hooks/useSpeechRecognition.js
+++ frontend/SqueekoApp/src/hooks/useSpeechRecognition.js
@@ -7,9 +7,37 @@ export const useSpeechRecognition = () => {
     const [ transcript, setTranscript ] = useState('')
     const [ error, setError ] = useState('')
 
-    const requestAudioPermission = async () => {
-        
-        // Get user permission on android
-        const requestAudioPermission = async () => { }
+    // Get user permission on android
+    const requestAudioPermission = async () => { 
+        if ( Platform.OS === 'android' ) {
+            try { 
+                const granted = await PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+                    {
+                        title: 'Squeeko Audio Permission',
+                        message: 'Squeeko needs access to your microphone for transcription',
+                        buttonNeutral: 'Ask Me Later',
+                        buttonNegative: 'Cancel',
+                        buttonPositive: 'OK'
+                    },
+                )
+                return granted === PermissionsAndroid.RESULTS.GRANTED
+            } catch (err) {
+                console.warn(err)
+                return false
+            }
+        }
+        // iOS permissions handled automatically when first used
+        // default granted permission for iOS
+        return true
     }
+
+    const onSpeechStart = useCallback( (e) => {
+        console.log('onSpeechStart:', e)
+        setIsListening(true)
+        setTranscript('')
+        setError('')
+    }, [])
+
+
 }
