54b00a7b6c75b57b267b561c01316a661175d3db
Raza Hanif <raza_hanif@ymail.com>
2025-04-17 17:55:49 -0400

    Working on Summarization @ 5:55:48 PM

--- backend-python/test_whisper.py
+++ backend-python/test_whisper.py
@@ -1,7 +1,16 @@
+from dotenv import load_dotenv
+import os
 import whisper
 
-model = whisper.load_model("tiny")
-result = model.transcribe("./audio/test.mp3")
+load_dotenv()
+whisper_model = os.getenv("WHISPER_MODEL", "tiny")
+
+# Declare device cpu & fp16 false - cuz intel sucks - this will change for prod 
+""" 
+for production the values change to: load_model(") & transcribe(audioPath)
+"""
+model = whisper.load_model(whisper_model, device="cpu")
+result = model.transcribe("./audio/test.mp3", fp16=False)
 
 print(result["text"])
 
