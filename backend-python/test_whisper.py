5af3b4e04024a1950ba9071a93ada39fe190cd57
Raza Hanif <raza_hanif@ymail.com>
2025-04-17 18:18:44 -0400

    Working on Summarization @ 6:18:43 PM

--- backend-python/test_whisper.py
+++ backend-python/test_whisper.py
@@ -1,16 +1,24 @@
 from dotenv import load_dotenv
 import os
 import whisper
+import time
 
 load_dotenv()
 whisper_model = os.getenv("WHISPER_MODEL", "tiny")
+
+enAudio = "./audio/test_en.mp3"
+faAudio = "./audio/test_fa.mp3"
+
+start = time.time()
 
 # Declare device cpu & fp16 false - cuz intel sucks - this will change for prod 
 """ 
 for production the values change to: load_model(") & transcribe(audioPath)
 """
-model = whisper.load_model(whisper_model, device="cpu")
-result = model.transcribe("./audio/test.mp3", fp16=False)
+model = whisper.load_model("medium", device="cpu")
+result = model.transcribe(faAudio, language="fa", fp16=False, task="translate")
 
+end = time.time()
+
+print(f"\nTranscription took {end - start:2f} seconds")
 print(result["text"])
-
