18ef808554507c82c1c0004bd4d7124b2cd5baff
Raza Hanif <raza_hanif@ymail.com>
2025-04-17 18:48:00 -0400

    Finished implementation of STT

--- backend-python/test_whisper.py
+++ backend-python/test_whisper.py
@@ -9,14 +9,12 @@ whisper_model = os.getenv("WHISPER_MODEL", "tiny")
 enAudio = "./audio/test_en.mp3"
 faAudio = "./audio/test_fa.mp3"
 
-print("Loading in Model")
 start = time.time()
 
 # Declare device cpu & fp16 false - cuz intel sucks - this will change for prod 
 # for production the values change to: load_model(") & transcribe(audioPath)
 
 model = whisper.load_model(whisper_model, device="cpu")
-print("Model Loaded")
 result = model.transcribe(enAudio, fp16=False, task="translate")
 
 end = time.time()
