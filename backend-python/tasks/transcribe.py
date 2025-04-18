792fa3317e708e824f733f621238999ad07e0be4
Raza Hanif <raza_hanif@ymail.com>
2025-04-18 15:05:59 -0400

    Working on python backend logic @ 3:05:59 PM

--- backend-python/tasks/transcribe.py
+++ backend-python/tasks/transcribe.py
@@ -1,7 +1,28 @@
 # Get all the logic already created in test_whisper.py
+from dotenv import load_dotenv
+import os
+import whisper
+import time
+
+load_dotenv()
+whisper_model = os.getenv("WHISPER_MODEL", "tiny")
+
+# Audio files for testing
+enAudio = "./audio/test_en.mp3"
+faAudio = "./audio/test_fa.mp3"
+
+# Manually setting device -> CPU & FP16 -> False | Cuz intel mac sucks - will be changed in prod
+# for prod the values are: load_model("medium or large") & model.transcribe(audioPath, task="translate")
+
 
 async def run(audio_url: str) ->  str:
-    # Placeholder - replace with actual Whisper logic
-    return "Output from Whisper"
+    # Load model into cache
+    model = whisper.load_model(whisper_model, device="cpu")
+    
+    # Run transcribtion | locking to FP32 & translating any non english to english
+    result = model.transcribe(audio_url, fp16=False, task="translate")
+    
+    # Return only the transcribed text for now...
+    return result["text"]
 
 
