3303727cc73876c2ddddaefb93c79d7c32ed2ef2
Raza Hanif <raza_hanif@ymail.com>
2025-04-18 21:32:27 -0400

    Working on python backend logic @ 9:32:27 PM

--- backend-python/audio_preprocessing/utils.py
+++ backend-python/audio_preprocessing/utils.py
@@ -1,26 +1,39 @@
+from pydub import AudioSegment
 import os 
 
+SUPPORTED_FORMATS = ["mp3", "wav", "m4a", "acc", "ogg", "flac", "webm"]
+AUDIO_LOADERS = {
+    "mp3": AudioSegment.from_mp3,
+    "wav": AudioSegment.from_wav,
+    "m4a": AudioSegment.from_m4a,
+    "ogg": AudioSegment.from_ogg,
+    "flac": AudioSegment.from_flac,
+    "webm": AudioSegment.from_webm
+}
 
-# Returns duraction of audio file in seconds
-def get_audio_duration(file_path: str) -> float:
-    # TODO: use pydub to calculate duration
+# Returns duration of audio file in seconds
+def get_duration(file_path: str) -> float:
+    # First check if file is supported
     file_type = is_supported(file_path)
         
-    if file_type is not None:
-        return 
+    if file_type:
+        try:
+            audio = AUDIO_LOADERS[file_type](file_path)
+            return audio.duration_seconds
+        except Exception as e:
+            print(f"Error loading or processing '{file_path}': {e}")
+            return 0.0
     else:
-        return 0
+        print(f"Unsupported Audio format: {file_path}")
+        return 0.0    
 
 # Checks if audio file is in a supported format
-def is_supported(file_path: str) -> bool:
-    supported_formats = ["mp3", "wav", "m4a", "acc", "ogg", "flac", "webm", "mp4"]
-
-    # Get file type
+def is_supported(file_path: str) -> str | None:
     _, extension = os.path.splitext(file_path) 
     file_type = extension.lstrip('.').lower()
     
     # Check if file is supported
-    if file_type in supported_formats:
+    if file_type in SUPPORTED_FORMATS:
         return file_type
     else:
         return None
