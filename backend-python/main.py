ac880250258d0c642a49fa2f3b5da2621361d516
Raza Hanif <raza_hanif@ymail.com>
2025-04-19 12:44:31 -0400

    Working on diarization @ 12:44:31 PM

--- backend-python/main.py
+++ backend-python/main.py
@@ -51,21 +51,29 @@ async def require_auth(request: Request):
     return True
 
 
+# --- Routes
+
+@app.get("/")
+def root():
+    """ 
+    Basic root endpoint to check if the server is live
+    """
+    return {"message": "Squeeko backend is live!"}
+
 @app.post("/transcribe")
-async def transcribe_audio(data: AudioRequest, auth: bool = Depends(require_auth)):
+async def transcribe_audio(data: models.AudioRequest, auth: bool = Depends(require_auth)):
+    # Assume diari
+    
     result = await transcribe.run(data.audio_url)
     return {"transcript": result}
 
+
 @app.post("/diarize")
-async def diarize_audio(data: AudioRequest, auth: bool = Depends(require_auth)):
+async def diarize_audio(data: models.AudioRequest, auth: bool = Depends(require_auth)):
     result = await diarize.run(data.audio_url)
     return {"speaker_segments": result}
 
 @app.post("/summarize")
-async def summarize_audio(data: AudioRequest, auth: bool = Depends(require_auth)):
+async def summarize_audio(data: models.AudioRequest, auth: bool = Depends(require_auth)):
     result = await summarize.run(data.audio_url)
     return {"summary": result}
-
-@app.get("/")
-def root():
-    return {"message": "Server is live!"}
