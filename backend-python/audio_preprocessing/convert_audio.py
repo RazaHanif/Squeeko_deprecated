4535480e0133f5f0d9cca60d35a35f981bbf99c2
Raza Hanif <raza_hanif@ymail.com>
2025-04-18 20:45:02 -0400

    Working on python backend logic @ 8:45:02 PM

---
+++ backend-python/audio_preprocessing/convert_audio.py
@@ -0,0 +1,4 @@
+from pydub import AudioSegment
+import os
+
+# convert any audio format into wav for 
--- backend-python/tasks/transcribe.py
+++ backend-python/tasks/transcribe.py
@@ -4,6 +4,13 @@ import os
 import whisper
 import time
 
+from audio_preprocessing import (
+    chunk_audio,
+    convert_audio,
+    downscale_audio,
+    trim_silence
+)
+
 load_dotenv()
 whisper_model = os.getenv("WHISPER_MODEL", "tiny")
 
@@ -15,7 +22,12 @@ faAudio = "./audio/test_fa.mp3"
 # for prod the values are: load_model("medium or large") & model.transcribe(audioPath, task="translate")
 
 
-
+def prepare_audio(audio_url):
+    wav_path = convert_audio.to_wav(audio_url)
+    trimmed_path = trim_silence.apply(wav_path)
+    downsampled_path = downscale_audio.to_16k(trimmed_path)
+    chunks = chunk_audio.split(downsampled_path)
+    return chunks
 
 async def run(audio_url: str) ->  str:
     # Load model into cache
