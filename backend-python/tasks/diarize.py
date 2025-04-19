2510dd3c00e3e4ba440b8a7f97fc1e58ecbad8f5
Raza Hanif <raza_hanif@ymail.com>
2025-04-19 13:29:41 -0400

    Working on diarization @ 1:29:41 PM

--- backend-python/tasks/diarize.py
+++ backend-python/tasks/diarize.py
@@ -13,3 +13,16 @@ HUGGING_FACE_HUB_TOKEN = os.getenv("HUGGING_FACE_HUB_TOKEN")
 if HUGGING_FACE_HUB_TOKEN is None:
     print("Warning: HUGGING FACE HUB TOKEN IS NOT SET")
 
+DEVICE = "cuda" if torch.cuda.is_availble() else "cpu"
+
+# Model Loading (on FastAPI start)
+
+pyannote_pipeline_instance = None
+
+def load_pyannote_pipeline():
+    """ 
+    Loads the Pyannote pipeline into memory
+    Intended to be called on startup
+    """
+    
+    global p
