7aabbc49ef77308a3f9f442493838abc3293caa0
Raza Hanif <raza_hanif@ymail.com>
2025-04-19 13:27:51 -0400

    Working on diarization @ 1:27:51 PM

--- backend-python/tasks/diarize.py
+++ backend-python/tasks/diarize.py
@@ -2,3 +2,14 @@ import os
 import torch
 from pyannote.audio import Pipeline
 
+
+# --- Configure
+
+PYANNOTE_PIPELINE_NAME = "pyannote/speaker-diarization-3.1"
+# Requires a HuggingFaceHubToken
+HUGGING_FACE_HUB_TOKEN = os.getenv("HUGGING_FACE_HUB_TOKEN")
+
+# Check token
+if HUGGING_FACE_HUB_TOKEN is None:
+    print("Warning: HUGGING FACE HUB TOKEN IS NOT SET")
+
