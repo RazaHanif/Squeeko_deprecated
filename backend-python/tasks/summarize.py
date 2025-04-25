1d40be2cd23b2026986a818f789a0a54f747b58c
Raza Hanif <raza_hanif@ymail.com>
2025-04-25 11:45:04 -0400

    working on summarize @ 11:45:04 AM

--- backend-python/tasks/summarize.py
+++ backend-python/tasks/summarize.py
@@ -1,3 +1,18 @@
-async def run(transcript: str) -> str:
-    # Placeholder - replace with transforms summary
-    return "Output from transformer"
+import asyncio
+import os
+import torch
+import json
+import re
+
+
+from transformers import AutoModelForCasualLM, AutoTokenizer, BitsAndBytesConfig
+from llama_cpp import Llama
+
+# Getting token from env
+from dotenv import load_dotenv
+load_dotenv()
+
+# Select LLM Model
+LLM_MODEL_NAME = os.getenv("LLM_MODEL", "Mistral-7B-Instruct-v0.2")
+
+
