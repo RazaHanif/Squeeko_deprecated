9dcd3189373f5fca39d7110b54e0938bb5f207f2
Raza Hanif <raza_hanif@ymail.com>
2025-05-08 21:07:02 -0400

    building tests @ 9:07:02 PM

--- backend-python/utils/auth.py
+++ backend-python/utils/auth.py
@@ -1,5 +1,7 @@
 import os
 
+TEST_TOKEN = "BEARER test_123"
+
 async def verify_token(auth_header: str | None) -> bool:
     """ 
     Verifies the auth token from the header
@@ -9,4 +11,9 @@ async def verify_token(auth_header: str | None) -> bool:
         return False
     
     # --- Simple Test Token Check
+    if auth_header == TEST_TOKEN:
+        print("INFO: Test token accepted")
+        return True
+        
+        
     
