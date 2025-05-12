09da06b8684c0880fcf8556b7c17c11b276c303d
Raza Hanif <raza_hanif@ymail.com>
2025-05-12 19:29:52 -0400

    building tests @ 7:29:52 PM

--- backend-python/test_whisper.py
+++ backend-python/test_whisper.py
@@ -2,23 +2,28 @@ import os
 import whisper
 import time
 
-whisper_model = "medium"
+whisper_model = "base"
 
-enAudio = "./audio/test_en.mp3"
-faAudio = "./audio/test_fa.mp3"
+enAudio = "./tests/audio/test_en.mp3"
+faAudio = "./tests/audio/test_fa.mp3"
 
 start = time.time()
+print("Starting Transcription")
 
 # Declare device cpu & fp16 false - cuz intel sucks - this will change for prod 
 # for production the values change to: load_model(") & transcribe(audioPath)
 
+print("Loading model")
 model = whisper.load_model(whisper_model, device="cpu")
+print("Model Loaded")
+
+print("starting english test")
 enResult = model.transcribe(enAudio, fp16=False, task="translate")
+print("completed english test...starting farsi test")
 faResult = model.transcribe(faAudio, fp16=False, task="translate")
+print("completed farsi test")
 
 end = time.time()
-
-print(f"Transcription Completed: {end - start:2f} seconds")
 
 print(f"English Test File: {enResult['text']}")
 print(f"Persian Test File: {faResult['text']}")
