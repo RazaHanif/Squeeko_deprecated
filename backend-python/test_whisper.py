2397ad4829ea8025edce145f46c0d4f479380ed8
Raza Hanif <raza_hanif@ymail.com>
2025-05-12 19:23:59 -0400

    building tests @ 7:23:59 PM

--- backend-python/test_whisper.py
+++ backend-python/test_whisper.py
@@ -1,10 +1,8 @@
-from dotenv import load_dotenv
 import os
 import whisper
 import time
 
-load_dotenv()
-whisper_model = os.getenv("WHISPER_MODEL", "tiny")
+whisper_model = "medium"
 
 enAudio = "./audio/test_en.mp3"
 faAudio = "./audio/test_fa.mp3"
