342ffa1154c7bc5d88ad80d3aa2eea4e50a6717c
Raza Hanif <raza_hanif@ymail.com>
2025-05-16 21:30:16 -0400

    trying to test, fixing all bug issues @ 9:30:16 PM

---
+++ backend-python/tests/test_c.py
@@ -0,0 +1,26 @@
+import pytest
+from fastapi.testclient import TestClient
+from main import app
+import os
+from pathlib import Path
+
+from main import app
+
+from utils.auth import TEST_TOKEN
+
+@pytest.fixture(scope="session")
+def test_client():
+    with TestClient(app) as client:
+        yield client  # client is reused for all tests in this session
+
+def test_transcribe_english_audio(test_client):
+    with open("./tests/audio/test_en.mp3", "rb") as f:
+        files = {"audio_file": ("test_en.mp3", f, "audio/mp3")}
+        response = test_client.post("/transcribe", files=files, headers={"Authorization": TEST_TOKEN})
+    assert response.status_code == 200
+
+def test_transcribe_farsi_audio(test_client):
+    with open("./tests/audio/test_fa.mp3", "rb") as f:
+        files = {"audio_file": ("test_fa.mp3", f, "audio/mp3")}
+        response = test_client.post("/transcribe", files=files, headers={"Authorization": TEST_TOKEN})
+    assert response.status_code == 200
