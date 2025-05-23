9b4156921e775518b9be355a7ae9ff286cbc49e1
Raza Hanif <raza_hanif@ymail.com>
2025-05-23 18:09:54 -0400

    Starting node backend @ 6:09:54 PM

--- backend-node/middleware/auth.js
+++ backend-node/middleware/auth.js
@@ -1,1 +1,23 @@
 // Auth Middleware
+
+// Basic Imports
+import jwt from 'jsonwebtoken'
+import config from '../config'
+// Import userService for user validation?
+
+export const authenticateToken = (req, res, next) => {
+    // TODO: Get token from Auth header (Bearer <TOKEN>)
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) {
+        return res.status(401).json({ message: 'Authentication token is missing'})
+    }
+
+    jwt.verify(token, config.jwtSecret, (err, user) => {
+        if (err) {
+            
+        }
+    })
+
+}
