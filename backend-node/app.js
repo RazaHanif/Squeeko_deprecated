526cbe013f0d3487d5208d12fd2ecc4ce1cceb30
Raza Hanif <raza_hanif@ymail.com>
2025-05-23 10:21:07 -0400

    Starting node backend @ 10:21:07 AM

--- backend-node/app.js
+++ backend-node/app.js
@@ -1,15 +1,43 @@
-// Main bakend server file
+// Node Backend
+
 
 // Basic Imports
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+
+// Import Config
 require('dotenv').config()
+import config from './config'
+
+
+// Import Routes
+import authRoutes from './routes/auth'
+import jobRoutes from './routes/job'
+
+
+// Import Middleware
+import { errorHandler } from './middleware/error'
+import { authHandler } from './middleware/auth'
+
 const express = require('express')
 const app = express()
 
+
+// Just for testing
 const port = process.env.PORT || 3000
 
 
 // Middleware
 app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.use(morgan('dev'))
+
+// Routes
+app.use('/api/auth', authRoutes)
+app.use('/api/jobs', jobRoutes)
 
 // Routes
 const summaryRouter = require('./routes/summarize')
