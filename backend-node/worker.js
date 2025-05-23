ac28a536132e7b304f42f6b39c9b2c8e8bfd3c33
Raza Hanif <raza_hanif@ymail.com>
2025-05-23 10:34:44 -0400

    Starting node backend @ 10:34:44 AM

--- backend-node/worker.js
+++ backend-node/worker.js
@@ -1,1 +1,8 @@
 // Dedicated entry point for BullMQ workers
+
+// Basic Imports
+import { Queue, Worker } from 'bullmq'
+import Redis from 'ioredis'
+
+// Import config
+import config from './config'
