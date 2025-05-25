d5636c6944f05a6efebaa6f9afe30828b51faf00
Raza Hanif <raza_hanif@ymail.com>
2025-05-25 12:56:05 -0400

    Starting node backend @ 12:56:05 PM

--- backend-node/services/job.js
+++ backend-node/services/job.js
@@ -5,3 +5,20 @@ import { transcriptionQueue } from '../workers/queues'
 import * as assemblyAI from './assemblyAi'
 import * as deepL from './deepL'
 import * as openAI from './openAi'
+import * as alignment from '../utils/align'
+
+
+export const createJobRecord = async (userId, fileKey, originalFilename) => {
+    // TOOD: Check user's current usage quota before creating new job
+    //       if quota exceeded throw error
+    
+    const job = await prisma.job.create({
+        data: {
+            userId,
+            audioFileUrl: fileKey,
+            originalFilename,
+            status: 'QUEUED'
+        }
+    })
+    return job
+}
