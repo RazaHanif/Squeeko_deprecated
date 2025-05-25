a2d6cce40d8330ebe7fdd82c9619a76e2b001426
Raza Hanif <raza_hanif@ymail.com>
2025-05-25 12:46:56 -0400

    Starting node backend @ 12:46:55 PM

--- backend-node/services/auth.js
+++ backend-node/services/auth.js
@@ -9,7 +9,13 @@ const SALT_ROUNDS = 10
 
 export const registerUser = async (email, password, firstName, lastName) => {
     // Check if user with email already exists
-    // if (user) { error }
+    const userExists = await prisma.user.findUnique({
+        where: { email }
+    })
+
+    if (userExists) {
+        throw new Error('User already exists')
+    }
 
     const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
 
@@ -27,5 +33,34 @@ export const registerUser = async (email, password, firstName, lastName) => {
 }
 
 export const loginUser = async (email, password) => {
-    const user = await prisma.user.findUnique
+    const user = await prisma.user.findUnique({
+        where: { email }
+    })
+    if (!user) {
+        throw new Error('Invalid credentials') // Wanna keep it lowkey vauge
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash)
+    if (!isPasswordValid) {
+        throw new Error('Invalid credentials')
+    }
+
+    // TODO: Generate JWT token
+    const token = jwt.sign(
+        {
+            id: user.id,
+            email: user.email,
+        },
+        config.jwtSecret,
+        {
+            expiresIn: '1h' // Adjust later
+        }
+    )
+
+    // TODO: Update last_login_at in db
+
+    return { user, token }
 }
+
+// findUserById
+// updateUser
