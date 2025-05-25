3038f8d3436c0d5bab17ecc6962c9ac54b345b3f
Raza Hanif <raza_hanif@ymail.com>
2025-05-25 12:29:31 -0400

    Starting node backend @ 12:29:31 PM

--- backend-node/routes/auth.js
+++ backend-node/routes/auth.js
@@ -1,1 +1,15 @@
-// API Route for auth
+// API Routing for auth
+
+import express from 'express'
+import * as auth from '../controllers/auth'
+
+const router = express.Router()
+
+router.post('/register', auth.register)
+router.post('/login', auth.login)
+
+// refresh-token
+// logout
+// forgot-password
+
+export default router
