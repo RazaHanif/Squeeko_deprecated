094da004ceeb375e59a8a666e10a3ef8e172f9c3
Raza Hanif <raza_hanif@ymail.com>
2025-05-25 12:30:46 -0400

    Starting node backend @ 12:30:46 PM

--- backend-node/routes/job.js
+++ backend-node/routes/job.js
@@ -1,1 +1,8 @@
 // API route for jobs
+
+import express from 'express'
+import * as jobs from '../controllers/job'
+import { authenticateToken } from '../middleware/auth'
+
+const router = express.Router()
+
