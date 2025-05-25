5ba84f6f3bb3efca291ef85ac92c366eab081924
Raza Hanif <raza_hanif@ymail.com>
2025-05-25 12:53:00 -0400

    Starting node backend @ 12:53:00 PM

--- backend-node/services/job.js
+++ backend-node/services/job.js
@@ -1,1 +1,7 @@
 // Main job logic
+
+import prisma from '../db/prisma'
+import { transcriptionQueue } from '../workers/queues'
+import * as assemblyAI from './assemblyAi'
+import * as deepL from './deepL'
+import * as openAI from './openAi'
