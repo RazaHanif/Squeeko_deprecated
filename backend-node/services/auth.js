fa9a533a17e27613a1a4f556a979ec29e2fdd202
Raza Hanif <raza_hanif@ymail.com>
2025-05-25 12:39:00 -0400

    Starting node backend @ 12:38:59 PM

--- backend-node/services/auth.js
+++ backend-node/services/auth.js
@@ -6,3 +6,26 @@ import prisma from '../db/prisma'
 import config from '../config'
 
 const SALT_ROUNDS = 10 
+
+export const registerUser = async (email, password, firstName, lastName) => {
+    // Check if user with email already exists
+    // if (user) { error }
+
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+
+    const user = await prisma.user.create({
+        data: {
+            email,
+            passwordHash: hashedPassword,
+            firstName,
+            lastName
+        }
+    })
+    // Remove hashed password from user object
+    // user.data.passwordHash = '***' ??
+    return user
+}
+
+export const loginUser = async (email, password) => {
+    
+}
