0ac7c3d3c85522f02a5317d31cdfc1bbf1f531c3
Raza Hanif <raza_hanif@ymail.com>
2025-05-29 17:51:45 -0400

    Starting API Integrations @ 5:51:45 PM

--- backend-node/workers/jobProcessor.js
+++ backend-node/workers/jobProcessor.js
@@ -1,1 +1,43 @@
 // Func to process jobs from queue will go here
+
+import * as jobService from '../services/job'
+import * as assemblyaiService from '../services/assemblyAi'
+import * as deeplService from '../services/deepL'
+import * as openaiService from '../services/openAi'
+import * as alignment from '../utils/align'
+import prisma from '../db/prisma' 
+
+export const processJob = async (job) => {
+    const { jobId, fileKey } = job.data
+
+    try {
+        // Step 1
+        await prisma.job.update({
+            where: { id: jobId },
+            data: { status: 'PROCESSING_STT' }
+        })
+        const webhookUrl = `${config.appBaseUrl}/api/webhooks/assemblyai`
+        const assemblyaiResponse = await assemblyaiService.startTranscription(fileKey, webhookUrl)
+        
+        await prisma.job.update({
+            where: { id: jobId },
+            data: {
+                assemblyaiJobId: assemblyaiResponse.id,
+            }
+        })
+
+        // TODO: 
+
+
+    } catch (err) {
+        console.error(`Failed to process job ${jobId} at STT initiation step:`, err)
+        await prisma.job.update({
+            where: { id: jobId },
+            data: {
+                status: 'FAILED',
+                errorMessage: `STT initiation failed: ${err.message}`
+            }
+        })
+        throw err
+    }
+}
