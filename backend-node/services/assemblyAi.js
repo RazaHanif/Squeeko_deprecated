95e9029293b91ab51f98f144da1d7124f36f869f
Raza Hanif <raza_hanif@ymail.com>
2025-05-29 16:45:15 -0400

    Starting API Integrations @ 4:45:14 PM

--- backend-node/services/assemblyAi.js
+++ backend-node/services/assemblyAi.js
@@ -5,4 +5,25 @@ import config from '../config'
 
 const API_BASE_URL = 'https://api.assemblyai.com/v2'
 
- 
+export const startTranscription = async (audioUrl, webhookUrl) => {
+    try {
+        const response = await axios.post(
+            `${API_BASE_URL}/transcript`, 
+            {
+                audio_url: audioUrl,
+                speaker_labels: true,
+                webhook_url: webhookUrl,
+                audio_language_detection: true
+            },
+            {
+                headers: {
+                    'Authorization': config.assemblyAI.apiKey,
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+        
+    } catch (err) {
+        
+    }
+}
