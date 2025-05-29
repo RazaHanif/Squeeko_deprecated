42ff63385e52821295c901ba89d93c87e7d3ace3
Raza Hanif <raza_hanif@ymail.com>
2025-05-29 17:40:35 -0400

    Starting API Integrations @ 5:40:35 PM

--- backend-node/workers/queues.js
+++ backend-node/workers/queues.js
@@ -1,1 +1,17 @@
 // BullMQ queues logic
+
+import { Queue } from 'bullmq'
+import Redis from 'ioredis'
+import config from '../config'
+
+const connection = new Redis(config.redisUrl, {
+    maxRetriesPerRequest: null,
+    enableReadyCheck: false,
+})
+
+// IDK if this is good or not?
+export const transcriptionQueue = new Queue('transcriptionQueue', { connection })
+export const translationQueue = new Queue('translationQueue', { connection })
+export const summarizationQueue = new Queue('summarizationQueue', { connection })
+
+// 
