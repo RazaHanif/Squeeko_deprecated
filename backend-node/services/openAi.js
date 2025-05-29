d8cc23b2b21404152d8f418deb699159f321bdaf
Raza Hanif <raza_hanif@ymail.com>
2025-05-29 17:03:44 -0400

    Starting API Integrations @ 5:03:44 PM

--- backend-node/services/openAi.js
+++ backend-node/services/openAi.js
@@ -1,2 +1,25 @@
 // Main OpenAI Logic
 
+import OpenAI from "openai"
+import config from "../config"
+
+const openai = new OpenAI({
+    apiKey: config.openai.apiKey
+})
+
+export const getSummary = async (transcriptText) => {
+    try {
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'system',
+                    content: 'You are a helpful assistant that summarizes transcripts. Provide a main topic, '
+                }
+            ]
+        })
+    } catch (err) {
+        console.err('Error getting sumamry from OpenAi:', err.response?.data || err.message)
+        throw new Error('Failed to generate summary with OpenAI')
+    }
+}
